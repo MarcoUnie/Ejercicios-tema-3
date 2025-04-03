507ad675f8ff0d6c54288373ba7d16e41f9c3e7f
MarcoUnie <mmagecoyt@gmail.com>
2025-04-03 13:54:03 +0200

    tesoro secreto

--- Torre Hanoi.py
+++ Torre Hanoi.py
@@ -1,31 +1,33 @@
 def resolver_torre_hanoi(n):
     # Inicializamos las torres como listas
-    torre_origen = list(range(n, 0, -1))  # Torre inicial con los discos
+    torre_origen = list(range(n, 0, -1))  # Torre inicial con los piedras
     torre_auxiliar = []  # Torre auxiliar vacía
     torre_destino = []  # Torre destino vacía
 
     # Método recursivo para resolver la Torre de Hanoi
-    def mover_discos(cantidad, origen, destino, auxiliar):
+    def mover_piedras(cantidad, origen, destino, auxiliar):
         if cantidad == 1:
-            # Mover un disco directamente
-            disco = origen.pop()
-            destino.append(disco)
-            print(f"Mover disco {disco} de {origen} a {destino}")
+            # Mover un piedra directamente
+            piedra = origen.pop()
+            destino.append(piedra)
+            print(f"Mover piedra {piedra} de {origen} a {destino}")
         else:
-            # Mover n-1 discos a la torre auxiliar
-            mover_discos(cantidad - 1, origen, auxiliar, destino)
-            # Mover el disco restante a la torre destino
-            mover_discos(1, origen, destino, auxiliar)
-            # Mover los n-1 discos de la torre auxiliar a la torre destino
-            mover_discos(cantidad - 1, auxiliar, destino, origen)
+            # Mover n-1 piedras a la torre auxiliar
+            mover_piedras(cantidad - 1, origen, auxiliar, destino)
+            # Mover el piedra restante a la torre destino
+            mover_piedras(1, origen, destino, auxiliar)
+            # Mover los n-1 piedras de la torre auxiliar a la torre destino
+            mover_piedras(cantidad - 1, auxiliar, destino, origen)
 
     # Llamar al método recursivo
-    mover_discos(n, torre_origen, torre_destino, torre_auxiliar)
+    mover_piedras(n, torre_origen, torre_destino, torre_auxiliar)
 
     # Mostrar el estado final de las torres
     print("Torre origen:", torre_origen)
     print("Torre auxiliar:", torre_auxiliar)
     print("Torre destino:", torre_destino)
+    print("TOMA LO HAS CONSEGUIDO ESTE ES EL TESORO SECRETO:https://media.giphy.com/media/l0IynKSD9UkNZ88a4/giphy.gif?cid=790b7611i7wk5r666ep1ok6i17ivjueehqg8xkte2xcidymx&ep=v1_gifs_search&rid=giphy.gif&ct=g")
+
 
 # Ejemplo de uso
-resolver_torre_hanoi(74)
+resolver_torre_hanoi(3)
